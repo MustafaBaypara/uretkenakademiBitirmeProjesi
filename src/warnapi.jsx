fd8c00cdacc8a09dceacbeeba8053f8604f86812
Mustafa Baypara <94145699+MustafaBaypara@users.noreply.github.com>
2024-03-05 17:51:12 +0300

    Update warnapi.jsx

--- src/warnapi.jsx
+++ src/warnapi.jsx
@@ -4,7 +4,7 @@ function useWarnData() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch('https://80.253.246.249:58731/apiGET')
+    fetch('https://apichikitabot.net:58731/apiGET')
       .then(response => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
